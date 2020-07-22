5868799712d9e51dcde80134c13f30fe793b1935
yeji <yejisoft@gmail.com>
2020-07-22 18:13:26 +0900

    separate rows (for answer comments), add title_num and answer_num

--- crawler_hc_csv.py
+++ crawler_hc_csv.py
@@ -1,15 +1,22 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+
 # url 뒤에 붙을 숫자 범위
-ran = range(1, 11071)
+ran = range(1, 11075)
 url = "https://hashcode.co.kr/questions/"
+
 # url 뒤에 지정한 범위 내의 숫자를 붙여 배열에 저장
 url_list = []
 for r in ran:
     url_list.append(url + str(r))
-data_list = []
+
+data_list = []  # 전체 데이터 저장하는 리스트
+
+title_num = 1   # 글번호 저장하는 변수
+
 for url in url_list:
+
     # 진행 상황 파악하기 위해 링크 출력
     print(url)
     # 파싱 작업
@@ -25,59 +32,85 @@ for url in url_list:
     main_comment = soup.select(
         'body > div.main > div.content > div.content-wrap > div.center > div.comments-wrap > ul.comments > li.comment > span.comment-content')
     # 댓글이 존재할 경우
-    mcomlist = []
+    mcomlist = []   # 댓글 저장하는 리스트
     if main_comment:
         # 배열 형태인 댓글들을 text 형으로 전환
         for mcom in main_comment:
             mcomlist.append(mcom.text)
         # 댓글들을 구분해줄 문자열(<------->)을 사이에 추가한다.
         mcom_str = "\n\n<-------------------------------->\n\n".join(mcomlist)
-    # 답변이 존재하지 않을 경우 예외 처리
+    # 댓글이 존재하지 않을 경우 예외 처리
     if not main_comment:
         mcom_str = "No Comments Exist"
-    #답변 추출
-    answers = soup.select(
-        'ul.answers-list > li.answer-item > div.center')
-    anslist=[]
-    # 답변이 존재할 경우
-    if answers:
-        # 배열 형태인 답변들을 text 형으로 전환
-        for answer in answers:
-            #답변의 본문 부분을 파싱
-            answer_markdown=answer.find_all(attrs={'class':'markdown'})
-            #각각 답변에 대한 댓글 부분을 파싱
-            answer_comment=answer.find_all(attrs={'class':'comment-content'})
-            # 새로운 리스트에 본문과 그에대한 댓글을 병합(merge)
-            # 새로운 리스트에 본문추가
-            anslist.append(answer_markdown[0].text)
-            # 답변과 그에대한 댓글들을 구분해줄 문자열(<----댓글--->)을 사이에 추가한다.
-            anslist.append("\n\n<---------댓글----------->\n")
-            # 새로운 리스트에 댓글들 추가
-            for ans in answer_comment:
-                anslist.append(ans.text)
-            # 답변들을 구분해줄 문자열(*************)을 사이에 추가한다.
-            anslist.append('\n*************************************************************\n')
-        #전체 답변에 대한 리스트전체를 문자열로 형변환한다.
-        answer_str = "\n\n".join(anslist)
-    # 답변이 존재하지 않을 경우 예외 처리
-    if not answers:
-        answer_str = "No Answers Exist"
     # 태그 추출
     tag = soup.select(
         'body > div.main > div.content > div.content-wrap > div.center > div.question-tags')
-    if not tag :
+    # 태그 존재하지 않을 경우 예외 처리
+    if not tag:
         tag = "No Tags Exist"
-    # 추출한 데이터 삽입
-    for item in zip(title, contents, mcom_str, answer_str, tag):
-        data_list.append(
-            {
-                '제목': item[0].text,
-                '본문': item[1].text,
-                '본문 댓글': mcom_str,
-                '답변': answer_str,
-                '태그': item[4].text.replace('\n', '').replace('\t', '').replace('  ', '')
-            }
-        )
+
+    #답변 추출
+    answers = soup.select(
+        'ul.answers-list > li.answer-item > div.center')
+    # 답변이 존재하지 않을 경우 예외 처리
+    if not answers:
+        answer_str = "No Answers Exist"
+
+    # 답변이 존재할 경우
+    if answers:
+        answer_num = 0    # 답변 번호 저장하는 변수
+
+        for answer in answers:
+            # 답변번호 증가
+            answer_num += 1
+            # 답변의 본문 부분을 파싱
+            answer_markdown = answer.find_all(attrs={'class':'markdown'})
+            # 각각 답변에 대한 댓글 부분을 파싱
+            answer_comment = answer.find_all(attrs={'class':'comment-content'})
+
+            # 댓글이 없을 경우
+            if len(answer_comment) < 1:
+                answer_comment = "no comments"
+
+                for item in zip(title, contents, mcom_str, answer_markdown[0], answer_comment, tag):
+                    data_list.append(
+                        {
+                            '글번호': title_num,
+                            '제목': item[0].text,
+                            '본문': item[1].text,
+                            '본문 댓글': mcom_str,
+                            '답변': answer_markdown[0].text,
+                            '답변 번호': answer_num,
+                            '댓글': answer_comment,
+                            '태그': item[5].text.replace('\n', '').replace('\t', '').replace('  ', '')
+                        }
+                    )
+            # 댓글이 있을 경우
+            else:
+                for ans in answer_comment:
+                    # 텍스트화
+                    ans = ans.text
+
+                    for item in zip(title, contents, mcom_str, answer_markdown[0], ans, tag):
+                        data_list.append(
+                            {
+                                '글번호': title_num,
+                                '제목': item[0].text,
+                                '본문': item[1].text,
+                                '본문 댓글': mcom_str,
+                                '답변': answer_markdown[0].text,
+                                '답변 번호': answer_num,
+                                '댓글': ans,
+                                '태그': item[5].text.replace('\n', '').replace('\t', '').replace('  ', '')
+                            }
+                        )
+
+    # 글번호 증가
+    title_num += 1
+
+
+
+
 # 추출한 데이터 csv파일 형태로 저장
 data = pd.DataFrame(data_list)
 data.to_csv('crawling_hashcode.csv')
